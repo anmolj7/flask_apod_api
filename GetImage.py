994b956fa9fa131f4234d9b5a80525f410ba1705
EmperorAj <codingwithaj@gmail.com>
2020-01-06 00:48:12 +0530

    better looking version

--- GetImage.py
+++ GetImage.py
@@ -8,6 +8,7 @@ URL = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
 class Image:
     def __init__(self, json_data):
         self.Dict = json_data 
+        print(self.Dict)
         for key in self.Dict:
             setattr(self, key, self.Dict[key])
 
@@ -25,10 +26,11 @@ class Image:
 
 def get_json_data(str_date):
     #Date should be in the format YYYY-MM-DD
-    date = datetime.datetime.strptime(str_date, '%Y-%m-%d')
+    date = datetime.datetime.strptime(str_date, '%m/%d/%Y')
+    print(date)
     if date.date() > datetime.date.today() or date.date() < datetime.date(1995, 6, 16):
         return False #Since date can't be smaller than 16th june 1995, or more than today's date.
-    curr_url = URL + f'&date={str_date}'
+    curr_url = URL + f'&date={str(date.date())}'
     json = requests.get(curr_url).json()
     return json 
 
--- main.py
+++ main.py
@@ -1,48 +1,54 @@
 from flask import Flask, send_file, request, render_template, redirect
 import codecs
-from  GetImage import get_json_data, Image
-import io 
-import img2pdf
+from GetImage import get_json_data, Image
 import requests
 from flask_weasyprint import HTML, render_pdf
 
 app = Flask(__name__)
 
-def convert2pdf(img_bin):
-    return img2pdf.convert(img_bin)
-    
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("home.html")
 
-@app.route('/download', methods=['POST'])
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
+
+@app.route("/download", methods=["POST"])
 def download():
-    title = request.form['title']
-    explain = request.form['explain']
-    link = request.form['link']
-    date = request.form['date']
-    html = f"<html>{title} <img src=\"{link}\"> {explain}</html>"
-    print(html)
+    title = request.form["title"]
+    explain = request.form["explain"]
+    link = request.form["link"]
+    date = request.form["date"]
+    html = f'<html>{title} <img src="{link}" width="40%" height="40%"> {explain}</html>'
     return render_pdf(HTML(string=html), download_filename=f"{date}.pdf")
 
 
-@app.route('/image', methods=['POST'])
+@app.route("/image", methods=["POST"])
 def image():
-    date = request.form['date']
+    date = request.form["date"]
     print(date)
     data = get_json_data(date)
+    if not data or "code" in data:
+        return redirect('/image_for_date_not_avaiable') #Redirects to error 404.
     img = Image(data)
-    return render_template('image.html', link=img.url, title=img.title, explain=img.explanation, date=date)
+    return render_template(
+        "image.html", link=img.url, title=img.title, explain=img.explanation, date=date
+    )
 
 
 @app.errorhandler(404)
 def not_found(e):
     return str(e)
 
+
 @app.errorhandler(500)
 def not_found(e):
-    return str(e)
+    return str(e)+"\nMake sure you've entered the correct date."
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=6969   )
+    app.run(debug=False, port=6969)
+
